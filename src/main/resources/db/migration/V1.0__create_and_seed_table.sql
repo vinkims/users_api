8ee113017f23ddfb0242a926f28e6c642c605f6b
vinkims <vincekigen@gmail.com>
2023-12-02 14:18:15 +0300

    Switch to PostgreSQL database

--- src/main/resources/db/migration/V1.0__create_and_seed_table.sql
+++ src/main/resources/db/migration/V1.0__create_and_seed_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
-    id SMALLINT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NULL,
-    email VARCHAR(200) NOT NULL UNIQUE,
-    role VARCHAR(40) NOT NULL
+    "id" SMALLSERIAL PRIMARY KEY,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NULL,
+    "email" VARCHAR(200) NOT NULL UNIQUE,
+    "role" VARCHAR(40) NOT NULL
 );
 
 INSERT INTO users (first_name, last_name, email, role)
